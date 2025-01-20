76d48915a29774c9208e2c0ff2b61c7c0994a86d
Riccardo Santilli <riccardosantilli@live.it>
2025-01-20 11:16:59 +0100

    auth implement

---
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/auth/LoginRequest.java
@@ -0,0 +1,9 @@
+package it.epicode.EpicEnergyBE.auth;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/auth/Role.java
@@ -0,0 +1,6 @@
+package it.epicode.EpicEnergyBE.auth;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
