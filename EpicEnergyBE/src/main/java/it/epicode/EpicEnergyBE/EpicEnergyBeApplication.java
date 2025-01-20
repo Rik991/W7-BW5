f98f3940df3b0948f98d2ddcddee1534390fa6b8
Riccardo Santilli <riccardosantilli@live.it>
2025-01-20 10:55:23 +0100

    first commit

---
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/EpicEnergyBeApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.EpicEnergyBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyBeApplication.class, args);
+	}
+
+}
