bc86533a7765e6648be56425a1fa6cdf77f19e9e
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-01-21 17:41:25 +0100

    controllo ClienteRunner

--- EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/cliente/ClienteRunner.java
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/cliente/ClienteRunner.java
@@ -21,45 +21,47 @@ public class ClienteRunner implements ApplicationRunner {
     @Autowired
     private ClienteService clienteService;
 
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        Comune comuneLegale = comuneRepository.findByDenominazione("Marsala");
-        Comune comuneOperativo = comuneRepository.findByDenominazione("Catania");
+        if (clienteService.findAll().isEmpty()) {
+            Comune comuneLegale = comuneRepository.findByDenominazione("Marsala");
+            Comune comuneOperativo = comuneRepository.findByDenominazione("Catania");
 
-        Indirizzo indirizzoSedeLegale = new Indirizzo();
-        indirizzoSedeLegale.setVia("via Mazzini");
-        indirizzoSedeLegale.setCivico("26");
-        indirizzoSedeLegale.setLocalita("Marsala");
-        indirizzoSedeLegale.setCap("91025");
-        indirizzoSedeLegale.setComune(comuneLegale);
+            Indirizzo indirizzoSedeLegale = new Indirizzo();
+            indirizzoSedeLegale.setVia("via Mazzini");
+            indirizzoSedeLegale.setCivico("26");
+            indirizzoSedeLegale.setLocalita("Marsala");
+            indirizzoSedeLegale.setCap("91025");
+            indirizzoSedeLegale.setComune(comuneLegale);
 
-        Indirizzo indirizzoSedeOperativa = new Indirizzo();
-        indirizzoSedeOperativa.setVia("Via Roma");
-        indirizzoSedeOperativa.setCivico("50");
-        indirizzoSedeOperativa.setLocalita("Catania");
-        indirizzoSedeOperativa.setCap("95124");
-        indirizzoSedeOperativa.setComune(comuneOperativo);
+            Indirizzo indirizzoSedeOperativa = new Indirizzo();
+            indirizzoSedeOperativa.setVia("Via Roma");
+            indirizzoSedeOperativa.setCivico("50");
+            indirizzoSedeOperativa.setLocalita("Catania");
+            indirizzoSedeOperativa.setCap("95124");
+            indirizzoSedeOperativa.setComune(comuneOperativo);
 
-        ClienteDTO clienteDTO = new ClienteDTO();
+            ClienteDTO clienteDTO = new ClienteDTO();
 
-        clienteDTO.setRagioneSociale("Epicode SRL");
-        clienteDTO.setPartitaIva("A34456");
-        clienteDTO.setEmail("epicode@epicode.it");
-        clienteDTO.setDataInserimento(LocalDate.now());
-        clienteDTO.setDataUltimoContatto(LocalDate.of(2025, 01, 20));
-        clienteDTO.setFatturatoAnnuale(54200.0);
-        clienteDTO.setPec("epicode@epicode.it");
-        clienteDTO.setTelefono("444221");
-        clienteDTO.setEmailContatto("danilofumuso@gmail.com");
-        clienteDTO.setNomeContatto("Danilo");
-        clienteDTO.setCognomeContatto("Fumuso");
-        clienteDTO.setTelefonoContatto("4483291");
-        clienteDTO.setTipoCliente(TipoCliente.SRL);
-        clienteDTO.setSedeLegale(indirizzoSedeLegale);
-        clienteDTO.setSedeOperativa(indirizzoSedeOperativa);
+            clienteDTO.setRagioneSociale("Epicode SRL");
+            clienteDTO.setPartitaIva("A34456");
+            clienteDTO.setEmail("epicode@epicode.it");
+            clienteDTO.setDataInserimento(LocalDate.now());
+            clienteDTO.setDataUltimoContatto(LocalDate.of(2025, 01, 20));
+            clienteDTO.setFatturatoAnnuale(54200.0);
+            clienteDTO.setPec("epicode@epicode.it");
+            clienteDTO.setTelefono("444221");
+            clienteDTO.setEmailContatto("danilofumuso@gmail.com");
+            clienteDTO.setNomeContatto("Danilo");
+            clienteDTO.setCognomeContatto("Fumuso");
+            clienteDTO.setTelefonoContatto("4483291");
+            clienteDTO.setTipoCliente(TipoCliente.SRL);
+            clienteDTO.setSedeLegale(indirizzoSedeLegale);
+            clienteDTO.setSedeOperativa(indirizzoSedeOperativa);
 
-        clienteService.createCliente(clienteDTO, null);
-
+            clienteService.createCliente(clienteDTO, null);
+        }
     }
 }
