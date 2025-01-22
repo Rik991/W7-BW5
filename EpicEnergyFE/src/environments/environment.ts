48d640e8195adf5a903d371f5fe5709dfbb3318c
Riccardo Santilli <riccardosantilli@live.it>
2025-01-22 16:55:15 +0100

    environments fix

--- EpicEnergyFE/src/environments/environment.ts
+++ EpicEnergyFE/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   registerUrl: 'http://localhost:8080/api/auth/register',
   loginUrl: 'http://localhost:8080/api/auth/login',
-  clientiRegisterUrl: 'http://localhost:8080/api/clienti-completi',
+  clientiRegisterUrl: 'http://localhost:8080/api/clienti',
 };
