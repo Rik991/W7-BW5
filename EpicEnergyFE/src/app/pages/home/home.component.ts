44035684800de84380c7fa6a5a2d6647e7f4ac64
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-01-22 12:02:24 +0100

    impostazione home

--- EpicEnergyFE/src/app/pages/home/home.component.ts
+++ EpicEnergyFE/src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  name: string = 'Joseph';
+  profileImg: string =
+    'https://img.freepik.com/premium-vector/man-avatar-profile-picture-isolated-background-avatar-profile-picture-man_1293239-4866.jpg';
+  fattureArr: any;
+  clientiArr: any;
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
