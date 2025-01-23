b0b94247b15ea641e6a0511c359847162e210624
danilofumuso <danilofumuso@gmail.com>
2025-01-23 11:29:01 +0100

    fattura done

--- EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/fattura/Fattura.java
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/fattura/Fattura.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 @Table(name = "fatture")
 public class Fattura {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false)
