409678037e322feddb0d4680e445fbd4059d9be6
danilofumuso <danilofumuso@gmail.com>
2025-01-23 12:52:04 +0100

    cliente aggiornato

--- EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/cliente/ClienteController.java
+++ EpicEnergyBE/src/main/java/it/epicode/EpicEnergyBE/cliente/ClienteController.java
@@ -148,10 +148,10 @@ public class ClienteController {
     }
 
     @GetMapping("/search_by_ragione_sociale")
-    public ResponseEntity<Page<Cliente>> getClientiByRagioneSociale(
+    public ResponseEntity<Cliente> getClientiByRagioneSociale(
             @RequestParam("ragioneSociale") String ragioneSociale,
             Pageable pageable) {
-        return ResponseEntity.ok(clienteService.getClientiByRagioneSociale(ragioneSociale, pageable));
+        return ResponseEntity.ok(clienteService.getClienteByRagioneSociale(ragioneSociale));
     }
 
 }
