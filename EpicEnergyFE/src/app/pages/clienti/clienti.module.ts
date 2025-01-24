ccf145f535d1de00966cd8ef21a0d3ec453bea19
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-01-24 15:49:46 +0100

    pagina cliente

--- EpicEnergyFE/src/app/pages/clienti/clienti.module.ts
+++ EpicEnergyFE/src/app/pages/clienti/clienti.module.ts
@@ -6,17 +6,25 @@ import { ClientiComponent } from './clienti.component';
 import { RegisterClientiComponent } from './register-clienti/register-clienti.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
-import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDropdownModule,
+  NgbPaginationModule,
+} from '@ng-bootstrap/ng-bootstrap';
 import { DashboardClienteComponent } from './dashboard-cliente/dashboard-cliente.component';
 
 @NgModule({
-  declarations: [ClientiComponent, RegisterClientiComponent, DashboardClienteComponent],
+  declarations: [
+    ClientiComponent,
+    RegisterClientiComponent,
+    DashboardClienteComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     ClientiRoutingModule,
     SharedModule,
     NgbPaginationModule,
+    NgbDropdownModule,
   ],
 })
 export class ClientiModule {}
--- EpicEnergyFE/src/app/pages/clienti/dashboard-cliente/dashboard-cliente.component.ts
+++ EpicEnergyFE/src/app/pages/clienti/dashboard-cliente/dashboard-cliente.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientiService } from '../../../services/clienti.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { iCliente } from '../../../interfaces/i-clienti';
 
 @Component({
   selector: 'app-dashboard-cliente',
   templateUrl: './dashboard-cliente.component.html',
-  styleUrl: './dashboard-cliente.component.scss'
+  styleUrl: './dashboard-cliente.component.scss',
 })
-export class DashboardClienteComponent {
+export class DashboardClienteComponent implements OnInit {
+  cliente!: iCliente;
+  comuneLegale!: string;
+  comuneOperativo!: string;
+  id!: number;
+  constructor(
+    private clientiService: ClientiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')!;
+
+    this.id = parseInt(id);
+
+    this.route.params.subscribe((params) => {
+      this.clientiService.getClientiById(parseInt(id)).subscribe((res) => {
+        this.cliente = res;
+
+        let comuneLegale = JSON.stringify(res.sedeLegale.comune);
+        comuneLegale = JSON.parse(comuneLegale).denominazione;
+        this.comuneLegale = comuneLegale;
+
+        let comuneOperativo = JSON.stringify(res.sedeOperativa.comune);
+        comuneOperativo = JSON.parse(comuneOperativo).denominazione;
+        this.comuneOperativo = comuneOperativo;
+      });
+    });
+  }
+
+  deleteCliente() {
+    this.clientiService.deleteClienti(this.id).subscribe(() => {
+      this.router.navigate(['/clienti']);
+    });
+  }
+
+  editCliente() {
+    this.router.navigate(['/clienti/register', this.id]);
+  }
+
+  goBack() {
+    this.router.navigate(['/clienti']);
+  }
 }
